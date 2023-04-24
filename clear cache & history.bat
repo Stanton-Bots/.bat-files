26875cda0e7c0ddbd583cbf0eb6aacb39ce96dff
Stanton-Bots <128545116+Stanton-Bots@users.noreply.github.com>
2023-04-24 12:51:55 -0700

    Update clear cache & history.bat

--- clear cache & history.bat
+++ clear cache & history.bat
@@ -1,22 +1,21 @@
-@ECHO ON
+@ECHO OFF
 
 ipconfig /flushdns
 
-msg * "Please close Chrome first"
+msg * "Killing all browsers..."
 
-timeout /t 1
+timeout /t 2 /nobreak
+
+taskkill /im chrome.exe
+
+taskkill /im msedge.exe
+
+cd Desktop
 
 del .\appdata\Local\Google\Chrome\"User Data"\Default\History
 del .\appdata\Local\Google\Chrome\"User Data"\Default\Cache_Data
+del .\appdata\Local\Micrsoft\Edge\"User Data"\Default\History
+del .\appdata\Local\Micrsoft\Edge\"User Data"\Default\Cache_Data
 cls
 
-color 0a
-
-echo  _    _ _     _     _            _ 
-echo | |  | (_)   | |   | |          | |
-echo | |__| |_  __| | __| | ___ _ __ | |
-echo |  __  | |/ _` |/ _` |/ _ \ '_ \| |
-echo | |  | | | (_| | (_| |  __/ | | |_|
-echo |_|  |_|_|\__,_|\__,_|\___|_| |_(_)
-
-TIMEOUT /T 2 /NOBREAK
+msg * "Hey, your history and cache is clear now. It should work for edge too now!"
