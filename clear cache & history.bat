954ea83d4f46d1edb96e44a4a7d1107184757249
Stanton-Bots <128545116+Stanton-Bots@users.noreply.github.com>
2023-04-24 10:11:32 -0700

    Update clear cache & history.bat

--- clear cache & history.bat
+++ clear cache & history.bat
@@ -1,6 +1,22 @@
-@ECHO OFF
+@ECHO ON
+
 ipconfig /flushdns
-cd .\appdata\Local\Google\Chrome\"User Data"\Default
-del History
+
+msg * "Please close Chrome first"
+
+timeout /t 1
+
+del .\appdata\Local\Google\Chrome\"User Data"\Default\History
 del .\appdata\Local\Google\Chrome\"User Data"\Default\Cache_Data
+cls
+
+color 0a
+
+echo  _    _ _     _     _            _ 
+echo | |  | (_)   | |   | |          | |
+echo | |__| |_  __| | __| | ___ _ __ | |
+echo |  __  | |/ _` |/ _` |/ _ \ '_ \| |
+echo | |  | | | (_| | (_| |  __/ | | |_|
+echo |_|  |_|_|\__,_|\__,_|\___|_| |_(_)
+
 TIMEOUT /T 2 /NOBREAK
